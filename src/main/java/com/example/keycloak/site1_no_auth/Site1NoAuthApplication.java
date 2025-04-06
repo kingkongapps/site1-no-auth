113755f8b2b330e20cbf3b5b47803d3db2bf659c
inmokang <inmo.kang@gmail.com>
2025-04-07 05:21:19 +0900

    first commit

---
+++ src/main/java/com/example/keycloak/site1_no_auth/Site1NoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site1_no_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Site1NoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Site1NoAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloak/site1_no_auth/Site1NoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site1_no_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Site1NoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
